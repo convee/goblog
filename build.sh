aad82dacae24a44dbd3408969091496a4be45b1d
convee <convee.cn@gmail.com>
2021-09-07 22:05:00 +0800

    feat:build

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+COMMIT=$(git rev-parse --short HEAD)
+TIME=$(date +%Y%m%d%H)
+
+case "$1" in
+'build')
+  GOOS=linux GOARCH=amd64 go build -v -ldflags "-X main.Commit=$COMMIT"
+  ;;
+'tar')
+  tar zcvf blog"$TIME".tar.gz ../blog/*
+  ;;
+*)
+  echo "usage: $0 {build|tar}"
+  exit 1
+  ;;
+esac
